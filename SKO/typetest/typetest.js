c55ab9ae8191fa2fd7d3ee963eadedc009b0de56
samanthangsy <41715554+samanthangsy@users.noreply.github.com>
2020-08-13 10:10:11 +0800

    Add files via upload
    this is my typetest from the week 4 lesson

---
+++ SKO/typetest/typetest.js
@@ -0,0 +1,30 @@
+function preload() {
+//Ensure the .ttf or .otf font stored in the assets directory 
+//is loaded before setup() and draw() are called 
+MaisonNeue = loadFont('data/MaisonNeue-Medium.otf');
+
+}
+
+
+function setup() {
+createCanvas(600,600);
+background(220);
+fill(0);
+textSize(32);
+textFont(MaisonNeue);
+textAlign(CENTER);
+}
+
+
+function draw() {
+  textSize(32);
+  text('hello world', 300,300);
+  
+  textSize(8);
+  text('hello world', 300,100);
+  
+  textSize(50);
+  text('hello world', 300,500);
+  
+
+}
