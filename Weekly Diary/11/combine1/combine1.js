11a6ef82dddf1abbb6b0338f9c61538071c31af9
samanthangsy <41715554+samanthangsy@users.noreply.github.com>
2020-10-08 10:49:57 +0800

    Update combine1.js

--- Weekly Diary/11/combine1/combine1.js
+++ Weekly Diary/11/combine1/combine1.js
@@ -4,14 +4,13 @@ var valueX;
 var valueY; 
 var sketch1 = function(wb) 
   wb.setup = function() {
-    wb.createCanvas(windowWidth,windowHeight);
-  textSize(26);
-  textFont("Avenir");
-  textAlign(LEFT);
-  fill(255);
-  };
-
-  wb.draw = function() {
+  wb.createCanvas(windowWidth,windowHeight);
+  wb.textSize(26);
+  wb.textFont("Avenir");
+  wb.textAlign(LEFT);
+  wb.fill(255);
+    
+  //if this exists on all 5 sketches, do i put this on function setup()
     //Prompt 1
     fill(0);
     textSize(20)
@@ -21,23 +20,26 @@ var sketch1 = function(wb)
     fill(0);
     textSize(20)
     text('TYPE 1-5 FOR MORE', windowWidth/1.14,windowHeight/1.1) 
+  };
 
+  wb.draw = function() {
+    
     //Fill color according to mouseMoved()
-    fill(valueX, 255-valueY, 255-valueX); 
+    wb.fill(valueX, 255-valueY, 255-valueX); 
     
     //Set text size according to mouseMoved()
-    textSize(random(valueX, valueY));
+    wb.textSize(random(valueX, valueY));
     //slow down jitter rate for readability 
-    frameRate(15);
+    wb.frameRate(15);
     
-    text('OUR TAVERNS AND OUR METROPOLITAN STREETS, OUR OFFICES AND FURNISHED ROOMS, OUR RAILROAD STATIONS AND OUR FACTORIES APPEARED TO HAVE US LOCKED UP HOPELESSLY. THEN CAME THE FILM AND BURST THIS PRISON-WORLD ASUNDER BY THE DYNAMITE OF THE TENTH OF THE SECOND, SO THAT NOW, IN THE MIDST OF ITS FAR-FLUNG RUINS AND DEBRIS, WE CALMLY AND ADVENTUROUSLY GO TRAVELLING. -WALTER BENJAMIN', windowWidth/2, windowHeight/2); 
+    wb.text('OUR TAVERNS AND OUR METROPOLITAN STREETS, OUR OFFICES AND FURNISHED ROOMS, OUR RAILROAD STATIONS AND OUR FACTORIES APPEARED TO HAVE US LOCKED UP HOPELESSLY. THEN CAME THE FILM AND BURST THIS PRISON-WORLD ASUNDER BY THE DYNAMITE OF THE TENTH OF THE SECOND, SO THAT NOW, IN THE MIDST OF ITS FAR-FLUNG RUINS AND DEBRIS, WE CALMLY AND ADVENTUROUSLY GO TRAVELLING. -WALTER BENJAMIN', windowWidth/2, windowHeight/2); 
 
     //text('Our taverns and our metropolitan streets, our offices and furnished rooms, our railroad stations and our factories appeared to have us locked up hopelessly. Then came the film and burst this prison-world asunder by the dynamite of the tenth of the second, so that now, in the midst of its far-flung ruins and debris, we calmly and adventurously go travelling. -Walter Benjamin ', windowWidth/2, windowHeight/2); 
-    textAlign(CENTER);  
+    wb.textAlign(CENTER);  
       
     //circle cursor
-    fill('WHITE'); 
-    ellipse(mouseX, mouseY, 20, 20); 
+    wb.fill('WHITE'); 
+    wb.ellipse(mouseX, mouseY, 20, 20); 
 
   };
   
