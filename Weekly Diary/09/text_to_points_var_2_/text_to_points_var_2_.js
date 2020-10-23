14ccb4a7d353c547e15db922b0ef4595fc182832
samanthangsy <41715554+samanthangsy@users.noreply.github.com>
2020-10-24 04:55:48 +0800

    Add files via upload

---
+++ Weekly Diary/09/text_to_points_var_2_/text_to_points_var_2_.js
@@ -0,0 +1,48 @@
+//example of P5js Text-to-point function by Samantha Ng 
+//remix from https://p5js.org/reference/#/p5.Font/textToPoints
+//For CODEWORDS, September 2020
+
+
+
+
+function draw() {
+  
+}
+let font;
+function preload() {
+  font = loadFont('data/Lato-Hairline.ttf');
+}
+
+let points;
+let bounds;
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  stroke(0);
+  fill (255,51,51); //neon pink
+
+  points = font.textToPoints('You cannot prohibit the catastrophe,you must surf it! -Paul Virilio', 0, 0, 1, {
+    sampleFactor: 20, //affects the smoothness of the swaying from side to side 
+    simplifyThreshold: 0
+  });
+  bounds = font.textBounds('You cannot prohibit the catastrophe, you must surf it! -Paul Virilio', 0, 0, 1);
+ 
+
+}
+
+function draw() {
+  background(0);
+  beginShape();
+  translate(-bounds.x * width / bounds.w, -bounds.y * height / bounds.h);
+  for (let i = 0; i < points.length; i++) {
+    let p = points[i];
+    vertex(
+      p.x * width / bounds.w +
+        sin(5 * p.y / bounds.h + millis() / 5) * width / 10,
+      p.y * height / bounds.h
+    );
+
+ 
+  }
+  endShape(CLOSE);
+
+}
